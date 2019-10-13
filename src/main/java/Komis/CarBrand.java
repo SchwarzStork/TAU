6ada91c22ca9b25f34240dd5a9d93dc6576ccf69
Aleksander Mojzych <32591117+SchwarzStork@users.noreply.github.com>
2019-10-13 23:02:36 +0200

    Add files via upload

---
+++ src/main/java/Komis/CarBrand.java
@@ -0,0 +1,10 @@
+package Komis;
+
+public class CarBrand {
+    protected int id;
+    protected String brand;
+
+    public CarBrand(){
+
+    }
+
---
+++ src/main/java/Komis/CarModel.java
@@ -0,0 +1,9 @@
+package Komis;
+
+public class CarModel {
+    protected int id;
+    protected String ModelName;
+    protected String EngineModel;
+    protected String BodyType;
+    protected String GearboxType;
+}
