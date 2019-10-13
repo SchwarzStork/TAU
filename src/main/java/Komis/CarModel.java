ea20ea98eafb92aa08e0ff14567ab5cf746ba765
Aleksander Mojzych <32591117+SchwarzStork@users.noreply.github.com>
2019-10-14 01:34:16 +0200

    Delete CarModel.java

--- src/main/java/Komis/CarModel.java
+++
@@ -1,9 +0,0 @@
-package Komis;
-
-public class CarModel {
-    protected int id;
-    protected String ModelName;
-    protected String EngineModel;
-    protected String BodyType;
-    protected String GearboxType;
-}
