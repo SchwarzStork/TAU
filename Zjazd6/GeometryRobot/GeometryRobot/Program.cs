4727ce45a4de576d65d3a591ac26d81bfa4c9d92
Aleksander Mojzych <32591117+SchwarzStork@users.noreply.github.com>
2019-12-15 15:52:50 +0100

    Added program for robot framework testing

---
+++ Zjazd6/GeometryRobot/GeometryRobot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeometryRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wybierz ilość boków figury (3 lub 4)");
+
+        }
+    }
+}
---
+++ Zjazd6/GeometryRobot/GeometryRobot/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryRobot
+{
+    public enum TriangleType
+    {
+        Scalene = 1, // no two sides are the same length
+        Isosceles =  2, // only two sides are the same length
+        Equilateral = 3, // all sides have the same length
+        Error = 4 // sides cant create a triangle
+    }
+    class Triangle
+    {
+        public TriangleType TriangleType { get; private set; }
+        public int sideA { get; private set; }
+        public int sideB { get; private set; }
+        public int sideC { get; private set; }
+
+        public Triangle(int a, int b, int c)
+        {
+            sideA = a;
+            sideB = b;
+            sideC = c;
+            TriangleType = GetTriangleType(a, b, c);
+        }
+
+        public static TriangleType GetTriangleType(int a, int b, int c)
+        {
+            if( a<=0 || b<=0 || c <= 0)
+            {
+                return TriangleType.Error;
+            }
+            if (a == b && a == c || b == c && b == a || c == a && c == b)
+            {
+                return TriangleType.Equilateral;
+            }
+            else if (a == b || a == c || b == c)
+            {
+                return TriangleType.Isosceles;
+            }
+            else
+            {
+                return TriangleType.Scalene;
+            }
+        }
+    }
+}
